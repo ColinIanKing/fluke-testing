76219df8fb84bd77ef58cee9ff4661c60bfc5326
Colin Ian King <colin.king@canonical.com>
2011-12-02 13:03:32 +0000

    Clean up intel audio test
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- pm-tests/intel-audio-powersave-test/run-test.sh
+++ pm-tests/intel-audio-powersave-test/run-test.sh
@@ -1,14 +1,14 @@
 run_test()
 {
-	chmod -x /usr/lib/pm-utils/power.d/*
-	chmod +x /usr/lib/pm-utils/power.d/intel-audio-powersave
 
 	# play samples so driver can enter power save mode
 	(dd if=/dev/zero bs=1024 count=1 | aplay) >& /dev/null
 	
+	/usr/lib/pm-utils/power.d/intel-audio-powersave $1
+
 	${SENDTAG_BEGIN}
 	sleep ${SLEEP_DURATION}
 	${SENDTAG_END}
 
-	chmod +x /usr/lib/pm-utils/power.d/*
+	/usr/lib/pm-utils/power.d/intel-audio-powersave false
 }
