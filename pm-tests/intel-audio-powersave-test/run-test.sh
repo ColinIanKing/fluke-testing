399bfe07db327b6cb5f8aca48af02903d80985ab
Colin Ian King <colin.king@canonical.com>
2011-12-02 16:59:15 +0000

    Ensure we push data to audio subsystem to enable power saving
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- pm-tests/intel-audio-powersave-test/run-test.sh
+++ pm-tests/intel-audio-powersave-test/run-test.sh
@@ -3,12 +3,13 @@ run_test()
 
 	# play samples so driver can enter power save mode
 	(dd if=/dev/zero bs=1024 count=1 | aplay) >& /dev/null
-	
 	/usr/lib/pm-utils/power.d/intel-audio-powersave $1
+	(dd if=/dev/zero bs=1024 count=1 | aplay) >& /dev/null
 
 	${SENDTAG_BEGIN}
 	sleep ${SLEEP_DURATION}
 	${SENDTAG_END}
 
 	/usr/lib/pm-utils/power.d/intel-audio-powersave false
+	(dd if=/dev/zero bs=1024 count=1 | aplay) >& /dev/null
 }
