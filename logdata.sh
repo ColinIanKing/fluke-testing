d081464218b225230d4e9cb487003ed45c89dca7
Colin Ian King <colin.king@canonical.com>
2012-07-31 12:42:36 +0100

    Update and tidy logdata.sh
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- logdata.sh
+++ logdata.sh
@@ -1,28 +1,57 @@
 #!/bin/bash
+
+#
+#  Need to configure this up to the IP address of your Fluke meter
+#
 if [ -z $METER_ADDR ]; then
-	METER_ADDR=FLUKE_8846A_1811019.local
-	METER_ADDR=192.168.1.13
+	METER_ADDR=192.168.0.2
 fi
+
+#
+#  Fluke meter port (default)
+#
 if [ -z $PORT ]; then
         PORT=3490
 fi
+
+#
+#  Tag messages from client, receive on this port
+#
 if [ -z $TAGPORT ]; then
-	TAGPORT=3500
+	TAGPORT=9999
 fi
+
+#
+#  We need instrument lib's logmeter command to do the logging
+#
 if [ -z $LOGMETER ]; then
 	# assume instrument lib is cloned under this tree
         LOGMETER=`pwd`/instrument-lib/logmeter
 fi
+
+#
+#  Default 1 Hz sampling rate
+#
 if [ -Z $INTERVAL] ; then
 	INTERVAL=1.0
 fi
-SAMPLES=10000000
-LOG=samples.log
 
+#
+#  Number of samples
+#
+SAMPLES=10000000
+
+#
+#  Log file
+#
+LOG=samples.log
 if [ $# -eq 1 ]; then
 	LOG=$1
 fi
 
+#
+#  No intstrument-lib? Go and clone it
+#
 if [ ! -d instrument-lib ]; then
 	git clone git://kernel.ubuntu.com/ubuntu/instrument-lib
 fi
